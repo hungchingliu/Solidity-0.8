b48ce16de2f0520cbe8ba6c32e8acaa4f720d8c7
robin-h-liu <robin_h_liu@trendmicro.com>
2023-04-10 01:26:15 +0800

    feat: gas optimisation for Week5/TodoList.sol

--- Week5/TodoList.sol
+++ Week5/TodoList.sol
@@ -15,18 +15,33 @@ contract TodoList {
     // pass in task name by calldata and push the new task into task array
     // completed is initialize to false
     function create(string calldata _name) external {
+        // use memory in input string params: 69829 gas
+        // use calldata in input string params: 69550 gas
         tasks.push(Task(_name, false));
     }   
     
     // update task status by tid
-    function update(uint _tid, bool _completed) external {
+    function update(uint _tid, bool _completed) external payable {
+        // non payable 46281 gas
+        // payable 46257 gas
         tasks[_tid].completed = _completed;
     }
 
     // get task information by tid
     function get(uint _tid) external view returns(string memory name, bool completed) {
-        name = tasks[_tid].name;
-        completed = tasks[_tid].completed;
+        // name = tasks[_tid].name;
+        // completed = tasks[_tid].completed;
+        // 8387 gas
+
+        // Task memory task = tasks[_tid];
+        // name = task.name;
+        // completed = task.completed;
+        // 8300 gas
+
+        Task storage task = tasks[_tid];
+        name = task.name;
+        completed = task.completed;
+        // 8209 gas
     }
 
     // delete the contract
